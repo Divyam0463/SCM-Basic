c7412e9f9c1641db39244958caba8be4a72cbeb0
Divyam0463 <102290944+Divyam0463@users.noreply.github.com>
2022-05-17 21:31:37 +0530

    Added a .cpp file.

---
+++ a.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+// void Matrix(int &m, int &n){
+//     cout<<"the number of rows are "<<m;
+//     cout<<"the number of columns are "<<n;
+//     int i=0;
+//     for (int a[i] = 0; i < m; i++)
+//     {
+//         cin>>a;
+//         cout<<a;
+//     }
+//     for (int a[i] = 0; i < n; i++)
+//     {
+//         cin>>a;
+//         cout<<a<<endl;
+//     }
+// }
+
+
+
+// int main(){
+//     cout<<Matrix(3,4);
+
+// }
